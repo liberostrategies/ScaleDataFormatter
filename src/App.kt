8ef32d8c1d4b14eeed8cd7052b62c241f1cc9176
pink <pinky.ramos@denovix.xomc>
2021-02-21 11:17:32 -0500

    Cleanup: Add comments.

--- src/App.kt
+++ src/App.kt
@@ -16,6 +16,15 @@ const val IDX_VISCERAL_FAT = 6
 const val IDX_BMR = 7
 const val IDX_MUSCLE_MASS = 8
 
+/**
+ * This program takes the old wireless scale data from the 1byOne Android app
+ * and converts it to the new ArboLeaf app format. The 1byOne app did not provide
+ * a multi-day export.
+ *
+ * The input files and arboleaf sample data live in /data.
+ *
+ * The output file "datafile.csv" will be generated.
+ */
 fun main(args: Array<String>) {
     var dateCurr = -1
     var filename: String
